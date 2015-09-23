484b4d2b3cf19d58f94e94e1e2d9793efebfbfd9
wifang <william_w_fang@yahoo.com>
2015-09-23 06:41:56 -0400

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,65 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>William App</title>
-
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+<meta charset="UTF-8">
+<title>Roux Academy: Registration</title>
+<link href="{{ asset('_css/main.css') }}" rel="stylesheet" media="screen, projection">
+<meta name="viewport" content="initial-scale=1.0" />
+</script>
 </head>
-<body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-			</div>
-
-			<div class="collapse navbar-collapse" id="navbar">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Welcome</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if(auth()->guest())
-						@if(!Request::is('auth/login'))
-							<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						@endif
-						@if(!Request::is('auth/register'))
-							<li><a href="{{ url('/auth/register') }}">Register</a></li>
-						@endif
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-
-	@yield('content')
-
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<body id="blogPage">
+<header class="blogHeader pageHeader">
+  <h1>Roux Academy of Art and Design<a href="/index.htm" title="home"></a></h1>
+  <nav id="pageNav" class="cf">
+    <ul>
+      <li><a href="_source/programs/programs.htm" title="programs">Programs</a></li>
+      <li><a href="_source/admissions.htm" title="admissions">Admissions</a></li>
+      <li><a href="_source/student_portal.htm" title="student portal">Student Portal</a></li>
+      <li><a href="_source/campus_portal.htm" title="campus portal">Campus</a></li>
+      <li><a href="_source/alumni.htm" title="alumni">Alumni</a></li>
+      @if (Auth::check())
+      <li><a href="{{asset('auth/logout')}}"  title="Roux Academy Official logout">Logout</a></li>
+      @endif     
+      
+      <li><a href="_source/about/about.htm"  title="about Roux Academy">About</a></li>
+    </ul>
+  </nav>
+</header>
+    
+@yield('content')
+<footer id="pageFooter" class="cf">
+  <nav class="footerNav">
+    <section class="col">
+      <h3>About Roux Academy</h3>
+      <div class="col1">
+        <ul>
+          <li><a href="_source/mission.htm" title="Our mission">Mission Statement</a></li>
+          <li><a href="_source/history.htm" title="school history">School History</a></li>
+          <li><a href="_source/accreditation.htm" title="accreditation and affliates">Accreditation &amp; Affiliates</a></li>
+          <li><a href="_source/board.htm" title="board members">Board Members</a></li>
+        </ul>
+      </div>
+      <div class="col2">
+        <ul>
+          <li><a href="_source/faculty.htm" title="faculty and staff">Faculty &amp; Staff</a></li>
+          <li><a href="_source/visiting_professors.htm" title="visiting professors">Visiting Professors</a></li>
+          <li><a href="_source/museum.htm" title="Maribielle Roux Museum">Marbielle Roux Museum</a></li>
+          <li><a href="_source/directions.htm" title="directions">Map &amp; Directions</a></li>
+        </ul>
+      </div>
+    </section>
+    <section class="col">
+      <h3>Admissions &amp; Programs</h3>
+      <div class="col1">
+        <ul>
+          <li><a href="_source/degrees.htm" title="Degree programs">Degree Programs</a></li>
+          <li><a href="_source/catalog.htm" title="course catalog">Course Catalog</a></li>
+          <li><a href="_source/fine_art.htm" title="fine art programs">Fine Art Programs</a></li>
+          <li><a href="_source/design.htm" title="design concentration">Design Concentration</a></li>
+        </ul>
+      </div>
+      <div class="col2">
+        <ul>
+          <li><a href="_source/fashion.htm" title="fashion program">Fashion Program</a></li>
+          <li><a href="_source/product_design.htm" title="product design">Product Design</a></li>
+          <li><a href="_source/continuing_ed.htm" title="Continuing Education">Continuing Education</a></li>
+          <li><a href="_source/financial_aid.htm" title="tuition and financial aid">Tuition &amp; Financial Aid</a></li>
+        </ul>
+      </div>
+    </section>
+    <section class="col">
+      <h3>Student Resources</h3>
+      <div class="col1">
+        <ul>
+          <li><a href="_source/campus.htm" title="Campus information">Campus Information</a></li>
+          <li><a href="_source/housing.htm" title="student housing">Student Housing</a></li>
+          <li><a href="_source/contact.htm" title="transcript request">Transcript Request</a></li>
+          <li><a href="_source/applying.htm" title="application process">Application Process</a></li>
+        </ul>
+      </div>
+      <div class="col2">
+        <ul>
+          <li><a href="_source/portfolio_review.htm" title="portfolio review">Portfolio Review</a></li>
+          <li><a href="_source/conselling.htm" title="career counselling">Career Counselling</a></li>
+          <li><a href="_source/internships.htm" title="internship programs">Internship Programs</a></li>
+          <li><a href="_source/student_portal.htm" title="student portal login">Student Portal Login</a></li>
+        </ul>
+      </div>
+    </section>
+  </nav>
+  <p>&copy;Copyright  Roux Academy of Art &amp; Design.  All rights reserved. <a href="_source/privacy.htm" title="privacy statement">Privacy Statement</a> |<a href="_source/legal.htm" title="legal terms"> Legal Terms and Conditions</a> |<a href="_source/disclosures.htm" title="student outcomes and disclosures"> Student Outcomes/Disclosures</a></p>
+</footer>	
 </body>
 </html>

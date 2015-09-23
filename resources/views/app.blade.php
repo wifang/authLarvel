fc5269163881ffd07f802f7867341ac41befa526
wifang <william_w_fang@yahoo.com>
2015-09-23 07:07:13 -0400

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,11 +12,11 @@
   <h1>Roux Academy of Art and Design<a href="/index.htm" title="home"></a></h1>
   <nav id="pageNav" class="cf">
     <ul>
-      <li><a href="_source/programs/programs.htm" title="programs">Programs</a></li>
-      <li><a href="_source/admissions.htm" title="admissions">Admissions</a></li>
-      <li><a href="_source/student_portal.htm" title="student portal">Student Portal</a></li>
-      <li><a href="_source/campus_portal.htm" title="campus portal">Campus</a></li>
-      <li><a href="_source/alumni.htm" title="alumni">Alumni</a></li>
+      <li><a href="{{ url('/home') }}" title="programs">Programs</a></li>
+      <li><a href="{{ url('/home') }}" title="admissions">Admissions</a></li>
+      <li><a href="{{ url('/home') }}" title="student portal">Student Portal</a></li>
+      <li><a href="{{ url('/home') }}" title="campus portal">Campus</a></li>
+      <li><a href="{{ url('/home') }}" title="alumni">Alumni</a></li>
       @if (Auth::check())
       <li><a href="{{asset('auth/logout')}}"  title="Roux Academy Official logout">Logout</a></li>
       @endif     
@@ -33,18 +33,18 @@
       <h3>About Roux Academy</h3>
       <div class="col1">
         <ul>
-          <li><a href="_source/mission.htm" title="Our mission">Mission Statement</a></li>
-          <li><a href="_source/history.htm" title="school history">School History</a></li>
-          <li><a href="_source/accreditation.htm" title="accreditation and affliates">Accreditation &amp; Affiliates</a></li>
-          <li><a href="_source/board.htm" title="board members">Board Members</a></li>
+          <li><a href="{{ url('/home') }}" title="Our mission">Mission Statement</a></li>
+          <li><a href="{{ url('/home') }}" title="school history">School History</a></li>
+          <li><a href="{{ url('/home') }}" title="accreditation and affliates">Accreditation &amp; Affiliates</a></li>
+          <li><a href="{{ url('/home') }}" title="board members">Board Members</a></li>
         </ul>
       </div>
       <div class="col2">
         <ul>
-          <li><a href="_source/faculty.htm" title="faculty and staff">Faculty &amp; Staff</a></li>
-          <li><a href="_source/visiting_professors.htm" title="visiting professors">Visiting Professors</a></li>
-          <li><a href="_source/museum.htm" title="Maribielle Roux Museum">Marbielle Roux Museum</a></li>
-          <li><a href="_source/directions.htm" title="directions">Map &amp; Directions</a></li>
+          <li><a href="{{ url('/home') }}" title="faculty and staff">Faculty &amp; Staff</a></li>
+          <li><a href="{{ url('/home') }}" title="visiting professors">Visiting Professors</a></li>
+          <li><a href="{{ url('/home') }}" title="Maribielle Roux Museum">Marbielle Roux Museum</a></li>
+          <li><a href="{{ url('/home') }}" title="directions">Map &amp; Directions</a></li>
         </ul>
       </div>
     </section>
@@ -52,18 +52,18 @@
       <h3>Admissions &amp; Programs</h3>
       <div class="col1">
         <ul>
-          <li><a href="_source/degrees.htm" title="Degree programs">Degree Programs</a></li>
-          <li><a href="_source/catalog.htm" title="course catalog">Course Catalog</a></li>
-          <li><a href="_source/fine_art.htm" title="fine art programs">Fine Art Programs</a></li>
-          <li><a href="_source/design.htm" title="design concentration">Design Concentration</a></li>
+          <li><a href="{{ url('/home') }}" title="Degree programs">Degree Programs</a></li>
+          <li><a href="{{ url('/home') }}" title="course catalog">Course Catalog</a></li>
+          <li><a href="{{ url('/home') }}" title="fine art programs">Fine Art Programs</a></li>
+          <li><a href="{{ url('/home') }}" title="design concentration">Design Concentration</a></li>
         </ul>
       </div>
       <div class="col2">
         <ul>
-          <li><a href="_source/fashion.htm" title="fashion program">Fashion Program</a></li>
-          <li><a href="_source/product_design.htm" title="product design">Product Design</a></li>
-          <li><a href="_source/continuing_ed.htm" title="Continuing Education">Continuing Education</a></li>
-          <li><a href="_source/financial_aid.htm" title="tuition and financial aid">Tuition &amp; Financial Aid</a></li>
+          <li><a href="{{ url('/home') }}" title="fashion program">Fashion Program</a></li>
+          <li><a href="{{ url('/home') }}" title="product design">Product Design</a></li>
+          <li><a href="{{ url('/home') }}" title="Continuing Education">Continuing Education</a></li>
+          <li><a href="{{ url('/home') }}" title="tuition and financial aid">Tuition &amp; Financial Aid</a></li>
         </ul>
       </div>
     </section>
@@ -71,23 +71,24 @@
       <h3>Student Resources</h3>
       <div class="col1">
         <ul>
-          <li><a href="_source/campus.htm" title="Campus information">Campus Information</a></li>
-          <li><a href="_source/housing.htm" title="student housing">Student Housing</a></li>
-          <li><a href="_source/contact.htm" title="transcript request">Transcript Request</a></li>
-          <li><a href="_source/applying.htm" title="application process">Application Process</a></li>
+          <li><a href="{{ url('/home') }}" title="Campus information">Campus Information</a></li>
+          <li><a href="{{ url('/home') }}" title="student housing">Student Housing</a></li>
+          <li><a href="{{ url('/home') }}" title="transcript request">Transcript Request</a></li>
+          <li><a href="{{ url('/home') }}" title="application process">Application Process</a></li>
         </ul>
       </div>
       <div class="col2">
         <ul>
-          <li><a href="_source/portfolio_review.htm" title="portfolio review">Portfolio Review</a></li>
-          <li><a href="_source/conselling.htm" title="career counselling">Career Counselling</a></li>
-          <li><a href="_source/internships.htm" title="internship programs">Internship Programs</a></li>
-          <li><a href="_source/student_portal.htm" title="student portal login">Student Portal Login</a></li>
+          <li><a href="{{ url('/home') }}" title="portfolio review">Portfolio Review</a></li>
+          <li><a href="{{ url('/home') }}" title="career counselling">Career Counselling</a></li>
+          <li><a href="{{ url('/home') }}" title="internship programs">Internship Programs</a></li>
+          <li><a href="{{ url('/home') }}" title="student portal login">Student Portal Login</a></li>
         </ul>
       </div>
     </section>
   </nav>
-  <p>&copy;Copyright  Roux Academy of Art &amp; Design.  All rights reserved. <a href="_source/privacy.htm" title="privacy statement">Privacy Statement</a> |<a href="_source/legal.htm" title="legal terms"> Legal Terms and Conditions</a> |<a href="_source/disclosures.htm" title="student outcomes and disclosures"> Student Outcomes/Disclosures</a></p>
+  <p>&copy;Copyright  Roux Academy of Art &amp; Design.  All rights reserved. <a href="{{ url('/home') }}" title="privacy statement">Privacy Statement</a> |<a href="{{ url('/home') }}" title="legal terms"> Legal Terms and Conditions</a> |<a href="_source/disclosures.htm" title="student outcomes and disclosures"> Student Outcomes/Disclosures</a></p>
 </footer>	
 </body>
 </html>
+

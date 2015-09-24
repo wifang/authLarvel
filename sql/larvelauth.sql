99a4010a5b64f164a35239ffc43ae28eb8c4db4d
wifang <william_w_fang@yahoo.com>
2015-09-24 15:24:55 -0400

    Update larvelauth.sql

--- sql/larvelauth.sql
+++ sql/larvelauth.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2015 at 09:44 PM
+-- Generation Time: Sep 24, 2015 at 09:22 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -19,6 +19,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `larvelauth`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logincookies`
+--
+
+CREATE TABLE IF NOT EXISTS `logincookies` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `login_token` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logincookies`
+--
+
+INSERT INTO `logincookies` (`id`, `user_id`, `login_token`, `created_at`, `updated_at`) VALUES
+(10, 1, '19', '2015-09-24 21:29:26', '2015-09-24 21:29:26'),
+(11, 1, '94a08da1fecbb6e8b46990538c7b50b2', '2015-09-24 18:03:37', '2015-09-24 21:29:38');
 
 -- --------------------------------------------------------
 
@@ -51,6 +73,13 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('william_w_fang@yahoo.com', 'aa1791922281ce6e2608761dc6dd9152a46193057c59907594c97739e6f2d024', '2015-09-23 06:16:51');
+
 -- --------------------------------------------------------
 
 --
@@ -65,13 +94,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
+--
+-- Dumping data for table `users`
+--
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `logincookies`
+--
+ALTER TABLE `logincookies`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `password_resets`
@@ -90,10 +127,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `logincookies`
+--
+ALTER TABLE `logincookies`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
